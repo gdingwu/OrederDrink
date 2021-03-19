5fe2fea9bc5317b209f7538e55f4d4209c19275f
ding <anita_wu@9splay.com>
2021-03-19 15:34:36 +0800

    加入專案檔案。

---
+++ OrederDrink/Controllers/ChooseStoreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrederDrink.Controllers
+{
+    public class ChooseStoreController : Controller
+    {
+        private readonly ILogger<ChooseStoreController> _logger;
+
+        public ChooseStoreController(ILogger<ChooseStoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
